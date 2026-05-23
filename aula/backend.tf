212616327679ce0e041f01e9ac2d38bbc0c23e04
eliasipolito <eliasipolito@gmail.com>
2026-05-23 14:02:26 +0000

    Atualizando

--- aula/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform{
-    backend "s3" {
-        bucket = "terraform-state-eliasipolito"
-        key = "site/terraform.tfstate"
-        region = "us-east-2"
-        encrypt = true
-    }
-}
