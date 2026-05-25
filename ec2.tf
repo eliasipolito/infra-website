4ba8cb9fb762d713b8be32a9ada3aba0f640739a
eliasipolito <eliasipolito@gmail.com>
2026-05-25 14:48:53 +0000

    Atual

--- ec2.tf
+++ ec2.tf
@@ -55,6 +55,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 resource "aws_vpc_security_group_ingress_rule" "allow_all_outbound" {
     security_group_id = aws_security_group.website.id
     cidr_ipv4 = "0.0.0.0/0"
-    ip_protocol = -1
+    ip_protocol = "-1"
 
 }
